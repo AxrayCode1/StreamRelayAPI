52e39867983f083bfa6928c3ca0f53a0699fac10
AxrayCode1 <axray@hotmail.com>
2014-09-23 14:00:26 +0800

    API完成create、list、delete

---
+++ Process_API.php
@@ -0,0 +1,42 @@
+<?php
+class Process_API
+{
+    function create($jsondata)
+    {
+        $result = false;
+        $inputjson = json_decode($jsondata,true);
+        if(!(strlen($inputjson['Source']) <=0 || strlen($inputjson['Port']) <=0))
+        {
+            $exu_str = 'sudo /var/www/html/relay.exe ';
+            $exu_str.=$inputjson['Source'];
+            $exu_str.=' ';
+            $exu_str.=$inputjson['Port'];
+            $exu_str.=' ';
+            $exu_str.=$inputjson['ChannelName'];            
+            exec($exu_str,$output);         
+            $result = true;
+        }
+        return $result;
+    }
+    
+    function deldata($id)
+    {
+        $exu_str = "sudo /var/www/html/relay.exe stop $id";
+        exec($exu_str,$output);
+//        var_dump($output);       
+    }
+    
+    function listdata()
+    {
+        $ouputarr = null;
+        $exu_str = 'sudo /var/www/html/relay.exe list';            
+        exec($exu_str,$output);
+        foreach ($output as $str)
+        {
+            $str_arr=explode(" ",$str);
+            $ouputarr[] = array('id'=>$str_arr[0],'source'=>$str_arr[1],'dest'=>$str_arr[2]);           
+        }
+        return $ouputarr;
+    }
+}
+
