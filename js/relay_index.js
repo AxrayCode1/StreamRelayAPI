6dc73a1f07d249d06c4e42cfcce28cbb65bb095c
AxrayCode1 <axray@hotmail.com>
2014-09-25 11:01:41 +0800

    新增刪除，以及jquery busy plugin的使用

--- js/relay_index.js
+++ js/relay_index.js
@@ -40,7 +40,9 @@ function createAction()
 }
 
 var CreateHtml = {
-    createNew : function(){
+    createNew : function(input_relayobj){
+        var delete_class = '.delete';     
+        var relayobj_forhtml = input_relayobj;
         var table_relay = $('#table_relay');
         var createobj = {};
         createobj.clearTable = function(){
@@ -58,48 +60,69 @@ var CreateHtml = {
                 table_relay.append(append_str);                
              })            
         }       
+        createobj.rebindDeleteEvert = function(){
+            $(delete_class).click(function () {
+                var thisitem = $(this);
+                relayobj_forhtml.deleteRelay(thisitem.attr('id'));
+            });
+        };
+        createobj.blockPage = function(){
+            $.blockUI();
+        }
+        createobj.stopPage = function(){
+            $(document).ajaxStop($.unblockUI);
+        }
         return createobj;
     }
 }
 
 var Relay = {
-    createNew: function(){
-        var delete_class = '.delete';
-        var htmlobj = CreateHtml.createNew();
-        var relayobj = {}; 1       
+    createNew: function(){           
+        var relayobj = {};      
+        var htmlobj = CreateHtml.createNew(relayobj);
         relayobj.getRelayList = function(){ 
             var request = relayobj.CallAjax("relay/list","GET", '', "json");
-                request.done(function (msg, statustext, jqxhr)
-                {           
-                    htmlobj.clearTable();
-                    htmlobj.appendTable(msg);
-                    relayobj.rebindDeleteEvert();
-                });
-                request.fail(function (jqxhr, textStatus)
-                {            
-                    throw new MyError("Error : Ajax List Error" );
-                });
+            htmlobj.blockPage();
+            request.done(function (msg, statustext, jqxhr)
+            {                       
+                htmlobj.stopPage();
+                htmlobj.clearTable();
+                htmlobj.appendTable(msg);
+                htmlobj.rebindDeleteEvert();
+            });
+            request.fail(function (jqxhr, textStatus)
+            {            
+                htmlobj.stopPage();
+                throw new MyError("Error : Ajax List Error" );
+            });
         };
         relayobj.createRelay = function(source,port,channelname){ 
             var jsonrequest = '{"Source":"' + source + '","Port":' + port + ',"ChannelName":"' + channelname +  '"}';
             var request = relayobj.CallAjax("relay/create","POST", jsonrequest, "json");
+            htmlobj.blockPage();
             request.done(function (msg, statustext, jqxhr)
             {           
+                htmlobj.stopPage();
                 alert('Success');
                 relayobj.getRelayList();
             });
             request.fail(function (jqxhr, textStatus)
             {            
+                htmlobj.stopPage();
                 throw new MyError("Error : Ajax Create Error" );
             });
-        };
-        relayobj.rebindDeleteEvert = function(){
-            $(delete_class).click(function () {
-                var thisitem = $(this);
-                alert('delete');
-                alert(thisitem.attr('id'));
+        };        
+        relayobj.deleteRelay = function(deleteID){             
+            var request = relayobj.CallAjax("relay/delete/" + deleteID,"DELETE", "", "json");            
+            request.done(function (msg, statustext, jqxhr)
+            {                            
+                setTimeout(relayobj.getRelayList(),1000);                
             });
-        };
+            request.fail(function (jqxhr, textStatus)
+            {                            
+                throw new MyError("Error : Ajax Delete Error" );
+            });
+        };        
         relayobj.CallAjax = function(url,method, data, datatype){             
             var request = $.ajax({
                 type: method,
