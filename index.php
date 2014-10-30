d463da893af82824bc80dc8725f7f49245bee9f4
駱旻鴻 <childlok@outlook.com>
2014-10-30 17:18:41 +0800

    修改Login頁面排版

--- index.php
+++ index.php
@@ -12,60 +12,57 @@ if($oLogin->DB_Connection() == LoginStatus::DBConnectSuccess)
 }
 ?>
 
-<!doctype html>
-<head>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
 
-	<!-- Basics -->
-	
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
-	<title>AcroRed TV Relay Station Login</title>
+    <!-- Basics -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-	<!-- CSS -->
-	
-	<link rel="stylesheet" href="/css/reset.css">
-	<!--<link rel="stylesheet" href="css/animate.css">-->
-	<link rel="stylesheet" href="/css/login.css">
-        <script type='text/javascript' src="/js/sha512.js"></script>
-	<script type='text/javascript' src="/js/index.js"></script>
-</head>
+    <!--<meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>-->
 
-	<!-- Main HTML -->
-	
-<body>
-    
+    <title>AcroRed TV Relay Station Login</title>
+
+    <!-- CSS -->
+
+    <link rel="stylesheet" href="/css/reset.css"/>
+    <!--<link rel="stylesheet" href="css/animate.css"/>-->
+    <link rel="stylesheet" href="/css/login.css"/>
+    <script type='text/javascript' src="/js/sha512.js"></script>
+    <script type='text/javascript' src="/js/index.js"></script>
+  </head>
+
+  <!-- Main HTML -->
+
+  <body>
+
     <!-- Begin Page Content -->
-
-    <div id="container">
-			
-        <form action="/ui/Login.html" method="post" name="login_form">
-
+    <div id="login">
+        <div id="title">
+            TV Relay Server
+        </div>
+        <div id="container">
+            <form action="/ui/Login.html" method="post" name="login_form">
                 <label class="LabelHead" for="username">Username:</label>
-
-                <input type="text" id="username" name="username">
-
-                <label for="password">Password:</label>								
-
-                <input type="password" id="password" name="password">
-
+                <input type="text" id="username" name="username" />
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" />
                 <div id="lower">
-
-                        <!--<input type="checkbox"><label class="check" for="checkbox">Keep me logged in</label>-->
-
-                        <input type="submit" value="Login" onclick="formhash(this.form, this.form.password);">
-
+                    <!--<input type="checkbox"><label class="check" for="checkbox">Keep me logged in</label>-->
+                    <input type="submit" value="Login" onclick="formhash(this.form, this.form.password);">
                 </div><!--/ lower-->
-        </form>			
-    </div><!--/ container-->		
-	<!-- End Page Content -->	
-        <?php
+            </form>
+        </div><!--/ container-->
+    </div>
+    <!--/ container-->
+    <!-- End Page Content -->
+    <?php
             if (isset($_GET['error'])) {
                 echo '<script>alert("Login Failed.");window.location = "/ui/index.html"</script>';                
                 exit();
             }            
-    ?> 
-</body>
+    ?>
+  </body>
 
 </html>
-								
